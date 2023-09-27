cee4a7b82e0299d0a51c4d6e1d585497b5dfe063
tomo-kday <tomo.takebuchi@kkday.com>
2023-09-27 12:29:42 +0900

    feat: created footer navigation footer

---
+++ src/data/footer-navigation.ts
@@ -0,0 +1,61 @@
+// Todo href should be the whole path for now as there is only plan page.
+export const navigation = {
+	旅のおすすめ情報: [
+		{ name: "エリア一覧", href: "https://activityjapan.com/area/" },
+		{ name: "アクティビティ一覧", href: "https://activityjapan.com/activity/" },
+		{
+			name: "ACTIVITYJAPANBLOG一覧",
+			href: "https://activityjapan.com/column/",
+		},
+		{ name: "ACTIVITYJOURNAL一覧", href: "https://activityjapan.com/column/" },
+		{ name: "最新の特集一覧", href: "https://activityjapan.com/feature/" },
+		{
+			name: "オンライン体験一覧",
+			href: "https://activityjapan.com/feature/online-experiences/",
+		},
+	],
+	"国内のアクティビティの格安予約はアクティビティジャパンで！": [
+		{ name: "北海道", href: "https://activityjapan.com/search/hokkaido/" },
+		{ name: "東北", href: "https://activityjapan.com/search/tohoku/" },
+		{ name: "関東", href: "https://activityjapan.com/search/kanto/" },
+		{ name: "甲信越", href: "https://activityjapan.com/search/koushinetsu/" },
+		{ name: "北陸", href: "https://activityjapan.com/search/hokuriku/" },
+		{ name: "東海", href: "https://activityjapan.com/search/tokai/" },
+		{ name: "関西", href: "https://activityjapan.com/search/kansai/" },
+		{ name: "中国", href: "https://activityjapan.com/search/sanin-sanyo/" },
+		{ name: "四国", href: "https://activityjapan.com/search/shikoku/" },
+		{ name: "九州", href: "https://activityjapan.com/search/kyusyu/" },
+		{ name: "沖縄", href: "https://activityjapan.com/search/okinawa/" },
+	],
+	"アクティビティ事業者・法人向け": [
+		{
+			name: "サイトへの掲載について",
+			href: "https://activityjapan.com/publish_info",
+		},
+		{
+			name: "rezio（レジオ）-旅ナカ事業者用基幹システム",
+			href: "https://rezio.io/ja",
+		},
+	],
+	KKdayグループのサービスサイト: [
+		{ name: "KKday", href: "https://www.kkday.com/" },
+	],
+	"その他、旅行系サイト": [
+		{
+			name: "エイチ・エス損害保険",
+			href: "https://www.hs-sonpo.co.jp/kokunai/",
+		},
+		{
+			name: "HIS旅プロ -国内ホテル最安値予約-",
+			href: "https://www.his-vacation.com/",
+		},
+	],
+	"Q&A": [
+		{ name: "よくある質問", href: "https://activityjapan.com/faq" },
+		{ name: "ご利用規約", href: "https://activityjapan.com/accept/use/policy" },
+		{
+			name: "プライバシーポリシー",
+			href: "https://activityjapan.com/privacy/policy",
+		},
+	],
+};
