9559a8b45e4fd1e101af644889c7883c36ec74b6
tomo-kday <tomo.takebuchi@kkday.com>
2023-09-27 12:30:13 +0900

    feat: created an temporary entry page

---
+++ src/app/[lang]/publish/plan/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+	return (
+		<div>
+			mattew returning from Hokkido but go back again and go to Okinawa and goes
+			to Egypt for 悪魔を召喚する研究
+		</div>
+	);
+};
+
+export default Page;
